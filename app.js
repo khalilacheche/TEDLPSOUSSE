0c0b1a51f014854fc814a58a8eed8b44ee11e6cb
Amir Braham <amirbrahamm@gmail.com>
2017-06-19 17:24:20 +0100

    added more routes

--- app.js
+++ app.js
@@ -7,7 +7,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function (req,res) {
   res.render("Home");
 })
-
+app.get("/speakers",function (req,res) {
+  res.send("Speakers")
+})
+app.get("/talks",function (req,res) {
+  res.send("Talks")
+})
+app.get("/Team",function (req,res) {
+  res.send("Team")
+})
+app.get("/Sponsors",function (req,res) {
+  res.send("Sponsors");
+})
+app.get("/tedx2017/Program",function (req,res) {
+  res.send("tedx2017_program")
+})
+app.get("/tedx2017/signup",function (req,res) {
+  res.send("tedx2017_signUp")
+})
+app.get("/tedx2017/live",function (req,res) {
+  res.send("tedx2017_live")
+})
+app.get("/*",function (req,res) {
+  res.send("404");
+})
 
 app.listen(3000,function () {
   console.log("server started");
